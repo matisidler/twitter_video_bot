3dbeec2e621e1d06f918900210cb05dc432bb909
Matias <matisidler02@gmail.com>
2021-08-21 19:20:10 -0300

    make it better

--- funcs/func.go
+++ funcs/func.go
@@ -46,63 +46,60 @@ func getClient(creds *Credentials) (*twitter.Client, error) {
 var i = 1
 
 func Testing(ctx *fiber.Ctx) error {
-	for {
-
-		client, err := getClient(&Credentials{})
-		if err != nil {
-			return err
-		}
-		params := &twitter.MentionTimelineParams{Count: 1}
-
-		tweet, _, err := client.Timelines.MentionTimeline(params)
-		if len(tweet) == 0 {
-			fmt.Println("no mentions yet")
-		}
-
-		for _, tw := range tweet {
-			rpstr := tw.InReplyToStatusIDStr
-			rpint := tw.InReplyToStatusID
-			idint := tw.ID
-			twshow, _, err := client.Statuses.Show(rpint, nil)
-			if err != nil {
-				fmt.Println("not a reply")
-				break
-			}
-			name := tw.User.ScreenName
-			params := &twitter.StatusUpdateParams{InReplyToStatusID: idint}
-
-			if len(twshow.Entities.Media) == 0 {
-				_, _, err = client.Statuses.Update("@"+name+" ¿Sos estúpido? Esto no es un video", params)
-				if err != nil {
-					fmt.Println("Last step: ", err)
-				}
-				break
-			} else if len(twshow.Entities.Media) > 0 {
-				for _, i := range twshow.Entities.Media {
-					if strings.Contains(i.URLEntity.ExpandedURL, "photo") {
-						_, _, err = client.Statuses.Update("@"+name+" Esto es una foto, pavo.", params)
-						if err != nil {
-							fmt.Println("Last step: ", err)
-						}
-						break
-					} else if strings.Contains(i.URLEntity.ExpandedURL, "video") {
-						_, _, err = client.Statuses.Update("@"+name+` Acá tenés tu video. Disfrutalo! // Here is your download link! Enjoy it: https://ssstwitter.com/i/status/`+rpstr, params)
-
-						if err != nil {
-							fmt.Println("Last step: ", err)
-						}
-						break
-
-					}
-				}
-			}
-
-		}
-
-		time.Sleep(5 * time.Second)
-		i++
-		str := fmt.Sprintf("hola oleme las ", i)
-		_, err = ctx.WriteString(str)
+	client, err := getClient(&Credentials{})
+	if err != nil {
 		return err
 	}
+	params := &twitter.MentionTimelineParams{Count: 1}
+
+	tweet, _, err := client.Timelines.MentionTimeline(params)
+	if len(tweet) == 0 {
+		fmt.Println("no mentions yet")
+	}
+
+	for _, tw := range tweet {
+		rpstr := tw.InReplyToStatusIDStr
+		rpint := tw.InReplyToStatusID
+		idint := tw.ID
+		twshow, _, err := client.Statuses.Show(rpint, nil)
+		if err != nil {
+			fmt.Println("not a reply")
+			break
+		}
+		name := tw.User.ScreenName
+		params := &twitter.StatusUpdateParams{InReplyToStatusID: idint}
+
+		if len(twshow.Entities.Media) == 0 {
+			_, _, err = client.Statuses.Update("@"+name+" ¿Sos estúpido? Esto no es un video", params)
+			if err != nil {
+				fmt.Println("Last step: ", err)
+			}
+			break
+		} else if len(twshow.Entities.Media) > 0 {
+			for _, i := range twshow.Entities.Media {
+				if strings.Contains(i.URLEntity.ExpandedURL, "photo") {
+					_, _, err = client.Statuses.Update("@"+name+" Esto es una foto, pavo.", params)
+					if err != nil {
+						fmt.Println("Last step: ", err)
+					}
+					break
+				} else if strings.Contains(i.URLEntity.ExpandedURL, "video") {
+					_, _, err = client.Statuses.Update("@"+name+` Acá tenés tu video. Disfrutalo! // Here is your download link! Enjoy it: https://ssstwitter.com/i/status/`+rpstr, params)
+
+					if err != nil {
+						fmt.Println("Last step: ", err)
+					}
+					break
+
+				}
+			}
+		}
+
+	}
+
+	time.Sleep(5 * time.Second)
+	i++
+	str := fmt.Sprintln("hola oleme las ", i)
+	_, err = ctx.WriteString(str)
+	return err
 }
