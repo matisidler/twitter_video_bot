3599219ac12e5cdc20a1630b203285e7dbabce82
Matias <matisidler02@gmail.com>
2021-08-23 10:54:27 -0300

    version 1.0

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.16
 require (
 	github.com/dghubble/go-twitter v0.0.0-20210609183100-2fdbf421508e
 	github.com/dghubble/oauth1 v0.7.0
-	github.com/gofiber/fiber/v2 v2.17.0
 )
--- main.go
+++ main.go
@@ -1,15 +1,102 @@
 package main
 
 import (
-	"os"
-	"twit/server"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "5000"
-	}
-	server.Start(port)
+type Credentials struct {
+	AccessToken       string
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessTokenSecret string
+}
 
+func getClient(creds *Credentials) (*twitter.Client, error) {
+	config := oauth1.NewConfig("ConsumerKey", "ConsumerSecret")
+	token := oauth1.NewToken("AccessToken", "AccessTokenSecret")
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+
+	_, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+var i = 1
+
+func Testing() {
+	for {
+		client, err := getClient(&Credentials{})
+		if err != nil {
+			fmt.Println(err)
+		}
+		params := &twitter.MentionTimelineParams{Count: 10}
+
+		tweet, _, err := client.Timelines.MentionTimeline(params)
+		if len(tweet) == 0 {
+			fmt.Println("no mentions yet")
+		}
+
+		for _, tw := range tweet {
+			rpstr := tw.InReplyToStatusIDStr
+			rpint := tw.InReplyToStatusID
+			idint := tw.ID
+			twshow, _, err := client.Statuses.Show(rpint, nil)
+			if err != nil {
+				fmt.Println("not a reply")
+				break
+			}
+			name := tw.User.ScreenName
+			params := &twitter.StatusUpdateParams{InReplyToStatusID: idint}
+
+			if len(twshow.Entities.Media) == 0 {
+				_, _, err = client.Statuses.Update("@"+name+" ¿Sos estúpido? Esto no es un video", params)
+				if err != nil {
+					fmt.Println("Last step: ", err)
+				}
+				break
+			} else if len(twshow.Entities.Media) > 0 {
+				for _, i := range twshow.Entities.Media {
+					if strings.Contains(i.URLEntity.ExpandedURL, "photo") {
+						_, _, err = client.Statuses.Update("@"+name+" Esto es una foto, pavo.", params)
+						if err != nil {
+							fmt.Println("Last step: ", err)
+						}
+						break
+					} else if strings.Contains(i.URLEntity.ExpandedURL, "video") {
+						_, _, err = client.Statuses.Update("@"+name+` Acá tenés tu video. Disfrutalo! // Here is your download link! Enjoy it: https://ssstwitter.com/i/status/`+rpstr, params)
+
+						if err != nil {
+							fmt.Println("Last step: ", err)
+						}
+						break
+
+					}
+				}
+			}
+
+		}
+		time.Sleep(50 * time.Second)
+	}
+}
+
+func main() {
+	for {
+		Testing()
+		time.Sleep(50 * time.Second)
+	}
 }
